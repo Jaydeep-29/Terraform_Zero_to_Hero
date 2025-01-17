fc8ba5f75401ba6b98343a80a7ac5b8d4e05230c
Jaydeep-29 <jdpurohit29@gmail.com>
2025-01-17 00:08:48 +0000

    first commit

---
+++ Day 1/Project EC2 Creation/main.tf
@@ -0,0 +1,11 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-west-2" # set yor desire region
+}
+
+resource "aws_instance" "this" {
+  ami                     = "ami-0a89c0e1fe86ef74e" # specify appropriate AMI ID
+  instance_type           = "t2.micro"
+  subnet_id = "subnet-05de6470a5e4f1772"    # specify your subnet ID
+  key_name = "DemoKeyPair"   # specify your keypair
+}
