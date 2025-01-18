d6998bd0ecdfd74ebf7c347b5bccae51a9a77865
Jaydeep-29 <jdpurohit29@gmail.com>
2025-01-18 06:37:22 +0000

    update files and code for Day 3

--- Day 3/main.tf
+++ Day 3/main.tf
@@ -7,7 +7,7 @@ module "ec2_instance" {
     source = "./modules/ec2_instance"
     ami_value = "ami-0a89c0e1fe86ef74e" # replace this
     instance_type_value = "t2.micro"
-    subnet_id_value = "subnet-05de6470a5e4f1772". # replace this
+    subnet_id_value = "subnet-05de6470a5e4f1772" # replace this
 
   
 }
